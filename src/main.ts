986e63028e17329e7edbf2bb8d9b023b9dd14f76
Eduardo Golze <57068091+EduardoGolze@users.noreply.github.com>
2025-05-05 18:37:56 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT || 3000);
-  console.log(`Running on port ${process.env.PORT || 3000}`);
+  
+  // Configuração importante para o Railway
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on port ${port}`);
 }
 bootstrap();
