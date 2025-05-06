75b93002409897a2ec030603e7a35260fe4aa9bd
Eduardo Golze <57068091+EduardoGolze@users.noreply.github.com>
2025-05-06 20:45:55 -0300

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Sistema Siga API - Backend';
+    return 'Hello World!';
   }
 }
